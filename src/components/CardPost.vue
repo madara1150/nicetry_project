8c01cd0d9dcd26e4b5e73414b9c01f99f5612871
Tanathip64070045 <64070045@it.kmitl.ac.th>
2023-03-21 23:04:42 +0700

    create PostCard

---
+++ src/components/CardPost.vue
@@ -0,0 +1,186 @@
+<template>
+  <div>
+    <div class="w-full h-full">
+      <!-- profile -->
+      <div class="flex items-center bg-black p-2 rounded-t-xl w-full">
+        <img
+          src="https://tecdn.b-cdn.net/img/new/avatars/2.webp"
+          class="rounded-full"
+          style="width: 6vh"
+          alt="Avatar"
+        />
+        <h1 class="text-white ml-2 text-[2vh]">MADARA UCHIHA</h1>
+
+       
+      </div>
+
+      <!-- body -->
+      <div class="flex justify-center">
+        <div
+          class="flex flex-col rounded-b bg-white shadow-lg md:flex-row w-full"
+        >
+          <div class="flex flex-col justify-start p-6 w-full">
+            <!-- carousel -->
+            <div
+              id="carouselExampleCrossfade"
+              class="relative"
+              data-te-carousel-init
+              data-te-carousel-slide
+            >
+              <div
+                class="absolute inset-x-0 bottom-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0"
+                data-te-carousel-indicators
+              >
+                <button
+                  type="button"
+                  data-te-target="#carouselExampleCrossfade"
+                  data-te-slide-to="0"
+                  data-te-carousel-active
+                  class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+                  aria-current="true"
+                  aria-label="Slide 1"
+                ></button>
+                <button
+                  type="button"
+                  data-te-target="#carouselExampleCrossfade"
+                  data-te-slide-to="1"
+                  class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+                  aria-label="Slide 2"
+                ></button>
+                <button
+                  type="button"
+                  data-te-target="#carouselExampleCrossfade"
+                  data-te-slide-to="2"
+                  class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+                  aria-label="Slide 3"
+                ></button>
+              </div>
+              <div
+                class="relative w-full overflow-hidden after:clear-both after:block after:content-['']"
+              >
+                <div
+                  class="relative float-left -mr-[100%] w-full !transform-none opacity-0 transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none"
+                  data-te-carousel-fade
+                  data-te-carousel-item
+                  data-te-carousel-active
+                >
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
+                    class="block w-full"
+                    alt="Wild Landscape"
+                  />
+                </div>
+                <div
+                  class="relative float-left -mr-[100%] hidden w-full !transform-none opacity-0 transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none"
+                  data-te-carousel-fade
+                  data-te-carousel-item
+                >
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
+                    class="block w-full"
+                    alt="Camera"
+                  />
+                </div>
+                <div
+                  class="relative float-left -mr-[100%] hidden w-full !transform-none opacity-0 transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none"
+                  data-te-carousel-fade
+                  data-te-carousel-item
+                >
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
+                    class="block w-full"
+                    alt="Exotic Fruits"
+                  />
+                </div>
+              </div>
+              <button
+                class="absolute top-0 bottom-0 left-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+                type="button"
+                data-te-target="#carouselExampleCrossfade"
+                data-te-slide="prev"
+              >
+                <span class="inline-block h-8 w-8">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="h-6 w-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M15.75 19.5L8.25 12l7.5-7.5"
+                    />
+                  </svg>
+                </span>
+                <span
+                  class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                  >Previous</span
+                >
+              </button>
+              <button
+                class="absolute top-0 bottom-0 right-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+                type="button"
+                data-te-target="#carouselExampleCrossfade"
+                data-te-slide="next"
+              >
+                <span class="inline-block h-8 w-8">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="h-6 w-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                    />
+                  </svg>
+                </span>
+                <span
+                  class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                  >Next</span
+                >
+              </button>
+            </div>
+            <div class="flex pt-3">
+              <img
+                src="../image/heart-regular.svg"
+                alt=""
+                class="w-[5%] mx-2"
+              />
+              <img
+                src="../image/comment-regular.svg"
+                alt=""
+                class="w-[5%] mx-2"
+              />
+                <img
+                src="../image/triangle-exclamation-solid.svg"
+                alt=""
+                class="w-[5%] mx-2"
+              />
+              <img
+                src="../image/circle-dollar-to-slot-solid.svg"
+                alt=""
+                class="w-[5%] mx-2"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardPost",
+};
+</script>
+
+<style></style>
---
+++ src/components/Landing.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+
+    <div class="flex max-[600px]:flex-col">
+        <div class="w-[40%] max-[600px]:w-[100%]">
+            <CardPost />
+        </div>
+        <div class="ml-10">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur sunt eius a ea tempore! Voluptatibus eligendi voluptatum et ipsum iste.
+        </div>
+    </div>
+    
+    
+
+  </div>
+</template>
+
+<script>
+import CardPost from '../components/CardPost.vue';
+export default {
+    name:"Landing",
+    components:{
+        CardPost
+    }
+}
+</script>
+
+<style>
+
+</style>

d95ff4b6c8355732db03ab8c266f4de680e8c00f
Madara415 <88467996+Tanathip64070045@users.noreply.github.com>
2023-04-02 11:17:26 +0700

    update

--- src/components/NavUser.vue
+++ src/components/NavUser.vue
@@ -45,17 +45,44 @@
 
       <!-- credit  -->
       <div class="relative flex items-center">
-        <a class="max-[600px]:hidden mr-4 text-neutral-500 hover:text-red-500 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+        <a class="max-[600px]:hidden mr-4 text-neutral-500 hover:text-red-500 focus:text-neutral-700 disabled:text-black/30 [&.active]:text-black/90"
           href="#">
 
           <!-- btn credit -->
           <span class="w-5">
-            <button type="button"
+            <button type="button" data-te-toggle="modal" data-te-target="#exampleModal" data-te-ripple-init data-te-ripple-color="light"
               class="inline-block w-32 truncate rounded bg-[#F9FAFB] px-6 pt-2.5 pb-2 text-[2vh] font-medium uppercase leading-normal text-[#1F2937] shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-[#9FA6B2] hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-[#9FA6B2] focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]">
            {{ nice }} nice
             </button>
           </span>
         </a>
+        
+        <!-- modal -->
+        <div data-te-modal-init class="fixed top-0 left-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div data-te-modal-dialog-ref class="pointer-events-none relative w-auto translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px]">
+            <div class="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none">
+              <div class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4">
+                <h5 class="text-xl font-medium leading-normal text-neutral-800 " id="exampleModalLabel">
+                  Modal title
+                </h5>
+                <button type="button" class="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none" data-te-modal-dismiss
+                  aria-label="Close">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                </button>
+              </div>
+              <div class="relative flex-auto p-4" data-te-modal-body-ref>
+                Modal body text goes here.
+              </div>
+              <div class="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4">
+                <button type="button" class="inline-block rounded bg-primary-100 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200" data-te-modal-dismiss data-te-ripple-init data-te-ripple-color="light">
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
 
 
         <div class="relative">
@@ -66,7 +93,7 @@
               <ul class="absolute list-none w-[20em] bg-gradient-to-br from-black via-gray-800 to-rose-800 top-[100%] invisible right-0 rounded p-5 text-white" id="menu">
                 <li class="text-l p-3" id="menu-items"></li>
                 <li class="cursor-pointer rounded p-3 hover:bg-red-500 pb-2" id="menu-items"><a href="/register"><p class="text-2xl">Proflie</p></a></li>
-                <li class="text-l cursor-pointer rounded pl-3 p-1" id="menu-items"><a href="/login"><span class="text-l">{{nice}} nice.</span></a></li>
+                <li class="text-l cursor-pointer rounded pl-3 p-1" id="menu-items" data-te-toggle="modal" data-te-target="#exampleModal" data-te-ripple-init data-te-ripple-color="light"><a href="#"><span class="text-l">{{nice}} nice.</span></a></li>
                 <li class="text-2xl cursor-pointer rounded p-3 hover:bg-red-500" id="menu-items"><a href="">History</a></li>
                 <li class="text-2xl cursor-pointer rounded p-3 bg-red-400 hover:bg-red-800" id="menu-items"><a href="">Log out</a></li>
               </ul>
--- src/views/WithDraw.vue
+++ src/views/WithDraw.vue
@@ -1,14 +1,115 @@
 <template>
-    <div>
+    <div class="min-h-screen bg-[length:100%_120%] max-[600px]:min-h-max " style="background-image: url(https://cdn.discordapp.com/attachments/911172780781891614/1090455311028060170/pngwing.com.png)">
+        <NavUser />
 
-        madara
+    <div>
+        <h1 class="text-[3vh] text-center mt-5 max-[600px]:hidden" @click="clearSelect()">Please select the payment bank</h1>
+    </div>
+    
+    <div class="flex justify-center mt-5 max-[600px]:mt-1">
+
+        <!-- overflow banks -->
+        <div class="w-[80%] p-2 bg-gradient-to-br from-black via-gray-800 to-rose-800 overflow-x-scroll rounded max-[600px]:hidden">
+            <div class="flex w-[120%]">
+                <a v-for="val,i in bank" class="p-2 px-4 bg-white hover:bg-[#B8B0B0]" :key="i" :id="i" @click="CheckOutput(val.nice_name,i)">
+                    <img  :src="require('../image/th/'+val.img)" class="h-24 w-[100px] rounded-full" href="madara">
+                    <div class="w-[100px]">
+                    </div>
+                </a>
+            </div>
+        </div>
+
+       
+    </div>
+    
+    <div class="flex justify-center mt-10 max-[600px]:w-full max-[600px]:flex-col max-[600px]:p-5 max-[600px]:mt-5">
+        <form action="" class="w-[30%] max-[600px]:w-full">
+            <div>
+                <!-- Bank -->
+                
+                <!-- desktop -->
+                <h1 class="flex max-[600px]:hidden">BANK : <p class="text-red-500 pl-2">{{selectBank }}</p></h1>
+
+                <!-- phone -->
+                <select name="" id="" class="min-[600px]:hidden bg-gray-200 text-black mb-2 p-2 rounded w-full" v-model="selectBank">
+                    <option value="">Please Select Bank ...</option>
+                    <option :value="val.nice_name" v-for="val,i in bank" :key="i">{{ val.nice_name }}</option>
+                </select>
+
+                <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900">Bank Account name</label>
+                <input  v-model="nameAccount" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Name" required>
+                
+                <!-- bank number -->
+                <label for="first_name" class="block mb-2 mt-5 text-sm font-medium text-gray-900">Bank Account Number</label>
+                <input v-model="numberAccount" type="number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="number" required>
+                
+                <!-- amount -->
+                <label for="first_name" class="block mb-2 mt-5 text-sm font-medium text-gray-900">Amount</label>
+                <div class="flex max-[600px]:block">
+                    <input v-model="amountAccount" type="number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm w-40 rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 max-[600px]:w-full" placeholder="amount" required>
+                    <div>
+                        <button type="submit" class="text-white bg-black border border-gray-300 focus:outline-none hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-10 py-2.5 mr-2 mb-2 ml-5 max-[600px]:w-full max-[600px]:ml-0 max-[600px]:mt-5">Submit</button>
+                    </div>
+                </div>
+                
+            </div>
+        </form>
+
+        <!-- Receipt -->
+        <div class="w-[35%] ml-5 rounded p-16 text-gray-400 text-xl bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-black via-rose-900 to-black max-[600px]:w-full max-[600px]:p-5 max-[600px]:ml-0 max-[600px]:mt-5 max-[600px]:text-[90%]">
+            <h1 class="pb-2 text-white">Payment Receipt</h1>
+            <hr>
+            <h1 class="flex mt-2 text-[80%]">Account name : <p class="ml-2 text-white">{{ nameAccount }}</p></h1>
+            <h1 class="flex text-[80%]">Account number : <p class="ml-2 text-white">{{ numberAccount }}</p></h1>
+            <h1 class="flex text-[70%]">Bank : <p class="ml-2 text-white">{{ selectBank }}</p></h1>
+            <div class="pt-20">
+                <h1 class="flex justify-between pb-2">WithDraw <p class="justify-between"><span class="text-white">{{ amountAccount }}</span> Nice</p></h1>
+                <hr>
+            </div>
+        </div>
+
+    </div>
+   
+    
+
         
     </div>
 </template>
 
 <script>
+import NavUser from '@/components/NavUser.vue';
+import bank from '../data/banks.json'
 export default {
-    name :"WithDraw"
+    name :"WithDraw",
+    components:{
+        NavUser
+    },
+    data(){
+        return{
+            bank:bank,
+            selectBank:'',
+            nameAccount:'',
+            numberAccount:'',
+            amountAccount:0,
+        }
+    },
+    methods:{
+        CheckOutput(el,index){
+            console.log(el)
+            this.selectBank = el
+            this.clearSelect()
+            document.getElementById(index).style.backgroundColor = "#B8B0B0"
+         
+        },
+        clearSelect(){
+            let arr = Object.keys(bank)
+            for(let i =0 ;i<arr.length;i++){
+                document.getElementById(arr[i]).style.backgroundColor = "white"
+            }
+                
+        }
+    }
+    
 }
 </script>
 
